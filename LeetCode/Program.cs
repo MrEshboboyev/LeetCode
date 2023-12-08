91f36dd20f81f37b1aa632ee7f62e5a5825f616f
mreshboboyev <mreshboboyev@gmail.com>
2023-12-09 02:49:01 +0500

    1672. Richest Customer Wealth

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LeetCode;
+
 Console.WriteLine("Hello, World!");
+
+// this is 1672. Richest Customer Wealth
+int[][] accounts = { [2, 8, 7], [7, 1, 3], [1, 9, 5] };
+var customerWealth = new RichestCustomerWealth1672();
+Console.WriteLine("This is accounts[][] array : ");
+customerWealth.PrintArray(accounts);
+int result = customerWealth.MaximumWealth(accounts);
+Console.WriteLine($"Result : {result}");
---
+++ LeetCode/RichestCustomerWealth1672.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class RichestCustomerWealth1672
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            List<int> list = new List<int>();
+            for(int i = 0; i < accounts.Length; i++)
+            {
+                int sum = 0;
+                // Sum of the array elements
+                for(int j = 0; j < accounts[i].Length; j++)
+                {
+                    sum += accounts[i][j];
+                }
+                list.Add(sum);
+            }
+            return list.Max();
+        }
+
+        public void PrintArray(int[][] accounts)
+        {
+            // Display the array elements:
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                Console.Write("Element [" + i + "] Array: ");
+                for (int j = 0; j < accounts[i].Length; j++)
+                    Console.Write(accounts[i][j] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
