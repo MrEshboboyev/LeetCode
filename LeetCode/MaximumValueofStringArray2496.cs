0efc06e9e2ded22df2ef7af33218d239b81c7496
mreshboboyev <mreshboboyev@gmail.com>
2023-12-10 02:42:02 +0500

    2496. Maximum Value of String in an Array

---
+++ LeetCode/MaximumValueofStringArray2496.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class MaximumValueofStringArray2496
+    {
+        public int MaximumValue(string[] strs)
+        {
+            List<int> list = new List<int>();
+            foreach (string str in strs)
+            {
+                if(!int.TryParse(str, out int result))
+                {
+                    list.Add(str.Length);
+                }
+                else
+                {
+                    list.Add(result);
+                }
+            }
+            return list.Max();
+        }
+
+        public void IsLetters(string str)
+        {
+            if (int.TryParse(str, out int result))
+            {
+                Console.WriteLine($"Conversion successful. Result: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed.");
+            }
+        }
+
+        public int SumOfDigits(string str)
+        {
+            int sum = 0;
+            foreach (char c in str)
+            {
+                sum += (int)char.GetNumericValue(c);
+            }
+            return sum;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -3,10 +3,17 @@ using LeetCode;
 
 Console.WriteLine("Hello, World!");
 
-// this is 1672. Richest Customer Wealth
-int[][] accounts = { [2, 8, 7], [7, 1, 3], [1, 9, 5] };
-var customerWealth = new RichestCustomerWealth1672();
-Console.WriteLine("This is accounts[][] array : ");
-customerWealth.PrintArray(accounts);
-int result = customerWealth.MaximumWealth(accounts);
-Console.WriteLine($"Result : {result}");
+//// this is 1672. Richest Customer Wealth
+//int[][] accounts = { [2, 8, 7], [7, 1, 3], [1, 9, 5] };
+//var customerWealth = new RichestCustomerWealth1672();
+//Console.WriteLine("This is accounts[][] array : ");
+//customerWealth.PrintArray(accounts);
+//int result = customerWealth.MaximumWealth(accounts);
+//Console.WriteLine($"Result : {result}");
+
+// this is 2496. Maximum Value of a String in an Array
+string[] array = { "5232", "yv", "22", "c", "yawgs", "928", "4003", "2" };
+var maxValueStrArray = new MaximumValueofStringArray2496();
+Console.WriteLine(maxValueStrArray.MaximumValue(array));
+//maxValueStrArray.IsLetters("90a");
+//Console.WriteLine(maxValueStrArray.SumOfDigits("452"));
