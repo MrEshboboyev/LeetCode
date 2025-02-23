167c6f373f7e18a7b1c4fccf0f09ffde884cb332
MrEshbobotev <mreshboboyev@gmail.com>
2025-02-23 21:35:59 +0500

    2932. Maximum Strong Pair XOR I

---
+++ MaximumStrongPairXORI2932/MaximumStrongPairXor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MaximumStrongPairXORI2932;
+
+public static class MaximumStrongPairXORI2932_Solution
+{
+    public static int MaximumStrongPairXor(int[] nums)
+    {
+        int maxXor = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i; j < nums.Length; j++)
+            {
+                int x = nums[i];
+                int y = nums[j];
+
+                if (Math.Abs(x - y) <= Math.Min(x, y))
+                {
+                    int xorValue = x ^ y;
+                    if (xorValue > maxXor)
+                    {
+                        maxXor = xorValue;
+                    }
+                }
+            }
+        }
+
+        return maxXor;
+    }
+}
